0a1c78b9249ba32accbf6c23e86fffb07bb65fea
Octowl <alsaff1987@gmail.com>
2021-08-12 23:33:27 +0200

    @Octowl review

--- routes/users.js
+++ routes/users.js
@@ -11,6 +11,14 @@ const {
 router.param("userId", async (req, res, next, userId) => {
   const user = await userFetch(userId, next);
   if (user) {
+    /**
+     * @Octowl:
+     *
+     * Isn't passport already assigning `req.user` for the currently authenticated user.
+     * Consider giving this a different name. `req._user`? `req.foundUser`?
+     *
+     * This way you know "Who's asking for the user?" and "Which user are they asking for?"
+     */
     req.user = user;
     next();
   } else {
