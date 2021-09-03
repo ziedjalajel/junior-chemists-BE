5a5516cece6717950395b3219a54a99045f9ebe7
ziedjalajel <zied.jalajel1993@gmail.com>
2021-09-03 22:58:02 +0300

    still deploying

--- db/models/index.js
+++ db/models/index.js
@@ -10,7 +10,12 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(process.env[config.use_env_variable], {
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: true,
+    },
+  });
 } else {
   sequelize = new Sequelize(
     config.database,
